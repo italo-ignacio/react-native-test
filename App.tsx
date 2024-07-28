13eaf4f024768c3957b3ef021e81b7ac3a6cbafe
iti <italo.felipe.ignacio@hotmail.com>
2024-07-28 11:52:42 -0300

    gdsf

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
 // import { NativeBaseProvider } from 'native-base';
+import { BleManager } from 'react-native-ble-plx';
+import { Button, SafeAreaView } from 'react-native';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { Routes } from 'main/routes';
-import { SafeAreaView } from 'react-native';
 import { SplashScreen } from 'presentation/environment';
 import { fontsToImport } from 'presentation/style/fonts';
 
 // import { makeSQLiteDatabaseClientFactory } from 'main/factories/db';
 import { persister, store } from 'store';
+import { useEffect, useState } from 'react';
 import { useFonts } from '@expo-google-fonts/roboto';
-import { useState } from 'react';
 import type { FC } from 'react';
 
 const App: FC = () => {
@@ -26,11 +27,53 @@ const App: FC = () => {
     setIsLoading(false);
   }, 3000);
 
+  const [manager, setManager] = useState<BleManager | null>(null);
+  const [isScanning, setIsScanning] = useState(false);
+
+  useEffect(() => {
+    const bleManager = new BleManager();
+
+    setManager(bleManager);
+
+    return () => {
+      bleManager.destroy();
+    };
+  }, []);
+
+  const startScan = () => {
+    if (manager) {
+      setIsScanning(true);
+      manager.startDeviceScan(null, null, (error, device) => {
+        if (error) {
+          console.log(error);
+          setIsScanning(false);
+          return;
+        }
+
+        console.log('Dispositivo encontrado: ', device.name);
+
+        // Aqui você pode parar o escaneamento ou conectar ao dispositivo
+      });
+    }
+  };
+
+  const stopScan = () => {
+    if (manager) {
+      manager.stopDeviceScan();
+      setIsScanning(false);
+    }
+  };
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persister}>
         {/* <NativeBaseProvider theme={theme}> */}
         <SafeAreaView style={{ flex: 1 }}>
+          <Button
+            onPress={isScanning ? stopScan : startScan}
+            title={isScanning ? 'Parar Escaneamento' : 'Iniciar Escaneamento'}
+          />
+
           {!isLoading && fontsLoaded ? <Routes /> : <SplashScreen />}
         </SafeAreaView>
 
